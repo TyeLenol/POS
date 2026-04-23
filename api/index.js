6838eb9c8af96c960f06c2835d8c6b25dccfd937
Tyelenol <marilynandrews10@gmail.com>
2026-04-23 16:57:36 +0000

    hot

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
-// api/index.js
-export { default } from '../server/index.js'
+let handler
+
+try {
+  const mod = await import('../server/index.js')
+  handler = mod.default
+} catch (err) {
+  console.error('[api] Failed to load server:', err)
+  handler = (_req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(500).end(JSON.stringify({ message: 'Server failed to initialize', error: err.message }))
+  }
+}
+
+export default handler
